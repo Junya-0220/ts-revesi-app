acb4af1ebb7692814ff7e1e8d5fb8367a44b8922
Junya-0220 <junya010220@gmail.com>
2023-06-16 22:36:20 +0900

    method

--- training/class.ts
+++ training/class.ts
@@ -1,6 +1,18 @@
 class Fraction {
   constructor(private _num: number, private _denomi: number) {}
 
+  add(other: Fraction): Fraction {
+    const resultNumerator =
+      this._num * other._denomi + this._denomi * other._num;
+    const resultDenominator = this._denomi * other._denomi;
+
+    return new Fraction(resultNumerator, resultDenominator);
+  }
+
+  toString(): string {
+    return `${this._num}/${this._denomi}`;
+  }
+
   get numerator() {
     return this._num;
   }
@@ -15,3 +27,12 @@ console.log(f1.denominator);
 
 // f1.num = 3;
 console.log(f1.numerator);
+
+const f2 = new Fraction(3, 4);
+console.log(f2.toString());
+
+const result = f1.add(f2);
+
+console.log(result.toString());
+
+const errResult = r1.munus(f2);
